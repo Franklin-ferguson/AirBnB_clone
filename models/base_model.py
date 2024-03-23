26edb2501dbf3a67cf399a92970bfe7a135779ad
root <hayiborfranklin@gmail.com>
2024-03-23 08:53:59 -0700

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -34,6 +34,10 @@ class BaseModel:
                     setattr(self, key, value)
 
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+        
             models.storage.new(self)
         
 
