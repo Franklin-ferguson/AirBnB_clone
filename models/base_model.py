16dc97a4af77bacf717bcb666a58ae226006e29c
root <hayiborfranklin@gmail.com>
2024-03-23 09:54:52 -0700

    hi

--- models/base_model.py
+++ models/base_model.py
@@ -21,9 +21,6 @@ class BaseModel:
         """
 
         datetime_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
 
         if kwargs:
             for key, value in kwargs.items():
